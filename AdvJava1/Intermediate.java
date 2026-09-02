d6a423ce40440230e65cecbb83e65e7004dd5596
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-09-02 23:30:28 +0530

    throw keyword to throw a Exception wherever reuired explicitly and with a custom error message

--- AdvJava1/Intermediate.java
+++ AdvJava1/Intermediate.java
@@ -18,31 +18,49 @@ public class Intermediate{
         // System.out.println(res);
 
         //Exception Handling Using try Catch 
-        int i=0;
+        int i=20;
         int j=0;
-        int arr[] =  new int[5];
-        String str = null;
+        // int arr[] =  new int[5];
+        // String str = null;
 
-        try{
-            //j= 18/i; // critical statement
-            System.out.println(arr[5]);  // critical statement
-            //System.out.println(str.length()); // critical statement
+        // try{
+        //     //j= 18/i; // critical statement
+        //     System.out.println(arr[5]);  // critical statement
+        //     //System.out.println(str.length()); // critical statement
 
-        }
+        // }
 
-        // multiple catch blocks to handle different types of exception with customised messages
-        catch(ArithmeticException e){ // executes only when exception is thrown
-            System.out.println("Cannot divide by zero.");
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Be in your limits.");
+        // // multiple catch blocks to handle different types of exception with customised messages
+        // catch(ArithmeticException e){ // executes only when exception is thrown
+        //     System.out.println("Cannot divide by zero.");
+        // }
+        // catch(ArrayIndexOutOfBoundsException e){
+        //     System.out.println("Be in your limits.");
+        // }
+        // catch(Exception e){
+        //     System.out.println("Something Went Wrong. "+ e);
+        // }
+
+        // System.out.println(j);
+        // System.out.println("Exceptional Handling");
+
+
+        // throw keyword 
+        try {
+            j= 18/i;
+            if(j==0)
+                throw new ArithmeticException("I dont want to print zero."); // throw exception with custom message 
+        } 
+        catch (ArithmeticException e) {
+            j=18/1; // Handling exception with a solution 
+            System.out.println("Thats the default output. "+ e);
         }
         catch(Exception e){
-            System.out.println("Something Went Wrong. "+ e);
+            System.out.println("Something went Wrong. "+ e);
         }
-
         System.out.println(j);
-        System.out.println("Exceptional Handling");
+        System.out.println("Byee");
+
 
     }
     
