ea3ae48afae6886fe3ea197dd93d5299606cb3fe
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-09-02 22:18:07 +0530

    Exception Handling

--- AdvJava1/Intermediate.java
+++ AdvJava1/Intermediate.java
@@ -13,9 +13,23 @@ public class Intermediate{
         //obj.show(5);
 
         // lamda Expression with returning function 
-        K obj = (i,j) -> i+j; 
-        int res = obj.add(5, 4);
-        System.out.println(res);
+        // K obj = (i,j) -> i+j; 
+        // int res = obj.add(5, 4);
+        // System.out.println(res);
+
+        //Exception Handling Using try Catch 
+        int i=0;
+        int j=0;
+
+        try{
+            j= 18/i; // critical statement
+        }
+        catch(Exception e){ // executes only when exception is thrown
+            System.out.println("Something went wrong..");
+        }
+
+        System.out.println(j);
+        System.out.println("Exceptional Handling");
 
     }
     
