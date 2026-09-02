2fc1c37238ec413faed1a8fd9b82f9e88d8dd358
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-09-02 08:42:02 +0530

    Lamda Expressions with Functional Interface

--- AdvJava1/Intermediate.java
+++ AdvJava1/Intermediate.java
@@ -2,16 +2,21 @@ package AdvJava1;
 
 @FunctionalInterface
 interface K{
-    void show();
+    //void show(int i);
+    int add(int i , int j);
+
 }
 public class Intermediate{
     public static void main(String[] args) {
-        K obj = new K(){
-            public void show(){
-                System.out.println("in show");
-            }
-        };
-        obj.show();
+        //K obj =  (int i) -> System.out.println("in show "+ i); 
+        //K obj =  i -> System.out.println("in show "+ i); 
+        //obj.show(5);
+
+        // lamda Expression with returning function 
+        K obj = (i,j) -> i+j; 
+        int res = obj.add(5, 4);
+        System.out.println(res);
+
     }
     
 }
