f838bc5deb43c84d5a14e55fed01e7e7ffe6c66f
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-09-04 09:19:47 +0530

    Running two methods parallelly with threads and understanding start() and run() in context of threads

---
+++ AdvJava1/LearnThreads.java
@@ -0,0 +1,27 @@
+package AdvJava1;
+class A extends Thread {
+    public void run(){
+        for(int i=0;i<100;i++){
+            System.out.println("hi");
+        }   
+    }
+}
+class B extends Thread {
+    public void run(){
+        for(int i=0;i<100;i++){
+            System.out.println("Hello");
+        }
+    }
+}
+public class LearnThreads {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+
+        // these two lines of code run parallelly with the help of threads
+        obj1.start(); // starting thread execution with start() , it will call corresponding run()
+        obj2.start();
+
+    }
+    
+}
